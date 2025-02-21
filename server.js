fae3a2e15796d9ff056bcedc7582e04cc5fca289
Bara Al-Sedih <98843912+BaraSedih11@users.noreply.github.com>
2025-02-21 19:56:23 +0200

    adding .gitignore

--- server.js
+++ server.js
@@ -21,7 +21,10 @@ const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
 
 mongoose
-  .connect(MONGO_URI)
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
